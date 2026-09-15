bb6144cbb18ef7e2687fabff9c7b075952e337a4
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-09-15 14:26:14 +0200

    fix: logo fixed px size and not rem based

--- app/components/logo.vue
+++ app/components/logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center gap-1.5">
-    <LogoMark class="size-6" />
+    <LogoMark class="size-[24px]" />
     <div class="text-xl font-bold font-logo mt-1">Taco</div>
   </div>
 </template>
